4d98f6ec4313ceffad3bdb97ed7d3a409673bd7f
Aaron Bond <aaron.reid.bond@gmail.com>
2023-05-01 07:42:04 -0700

    js warmup 2

--- js/warmups.js
+++ js/warmups.js
@@ -1,7 +1,25 @@
 "use strict";
 
-function seven() {
-    return "S-E-V-E-N-!"
-}
+// 1
+// function seven() {
+//     return "S-E-V-E-N-!"
+// }
 
-console.log(seven());
+// console.log(seven());
+
+function ranNum (max, min){
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+function findFactors(x) {
+    let arr = []
+    if(typeof x === "number" && !isNaN(x)) {
+        for(let i = 1; i <= x; i++){
+            if (x % i === 0) {
+                arr.push(i)
+            }
+        } console.log(arr)
+    } else {
+        return false
+    }
+}
+findFactors(ranNum(500, 1));
