3b62226ba5cb8ddd9e092243a9702f7ae9b5c03b
Aaron Bond <aaron.reid.bond@gmail.com>
2023-05-01 14:42:08 -0700

    style.css file and directory

--- js/warmups.js
+++ js/warmups.js
@@ -7,19 +7,22 @@
 
 // console.log(seven());
 
-function ranNum (max, min){
-    return Math.floor(Math.random() * (max - min +1)) + min;
-}
-function findFactors(x) {
-    let arr = []
-    if(typeof x === "number" && !isNaN(x)) {
-        for(let i = 1; i <= x; i++){
-            if (x % i === 0) {
-                arr.push(i)
-            }
-        } console.log(arr)
-    } else {
-        return false
-    }
-}
-findFactors(ranNum(500, 1));
+// 2
+// function ranNum (max, min){
+//     return Math.floor(Math.random() * (max - min +1)) + min;
+// }
+// function findFactors(x) {
+//     let arr = []
+//     if(typeof x === "number" && !isNaN(x)) {
+//         for(let i = 1; i <= x; i++){
+//             if (x % i === 0) {
+//                 arr.push(i)
+//             }
+//         } console.log(arr)
+//     } else {
+//         return false
+//     }
+// }
+//
+// console.log(findFactors(ranNum(500, 1)));
+
