4ae23a6ee1c27f56b051404cacc9ce1deb5f04be
Aaron Bond <aaron.reid.bond@gmail.com>
2023-05-23 09:04:13 -0700

    added jquerry

---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.list').css('font-size', '20px')
+
+    $('.list, #h1FF, #pFF').css('background', 'yellow')
+
+    // $('.highlight').css('background', 'yellow')
+
+    // let contents = $('#pFF').html();
+    // alert(contents);
+
+    // alert( 'The DOM has finished loading!' );
+});
