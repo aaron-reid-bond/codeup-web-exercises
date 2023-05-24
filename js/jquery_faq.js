fecf500e977823f9a0469b57c627212fd6bf3694
Aaron Bond <aaron.reid.bond@gmail.com>
2023-05-24 14:25:14 -0700

    jquery traverse exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -7,3 +7,35 @@ $('#bt').on('click', function(){
 $('dt').on('click', function(){
     $(this).toggleClass('highlight')
 })
+
+$('#bt1').on('click', function(){
+    let lastLi1 = $('#national-parks').children()
+    let lastLi2 = $('#state-parks-texas').children()
+    let lastLi3 = $('#randomN').children()
+    lastLi1.last().toggleClass('highlight')
+    lastLi2.last().toggleClass('highlight')
+    lastLi3.last().toggleClass('highlight')
+})
+
+$('#nationalH').on('click', function(){
+    let bold1 = $('#national-parks').children()
+    $(this).each(function(){
+            bold1.toggleClass('bold')
+        })
+})
+$('#stateH').on('click', function(){
+    let bold2 = $('#state-parks-texas').children()
+    $(this).each(function(){
+        bold2.toggleClass('bold')
+    })
+})
+$('#randomH').on('click', function(){
+    let bold3 = $('#randomN').children()
+    $(this).each(function(){
+        bold3.toggleClass('bold')
+    })
+})
+
+$('li').on('click', function(){
+    $(this).parent().children().first().toggleClass('blue')
+})
