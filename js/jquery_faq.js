892f98dc1e3e0e3a701768bb1394150a2c2c7bd0
Aaron Bond <aaron.reid.bond@gmail.com>
2023-05-25 07:43:44 -0700

    jquery traverse exercise refactor

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -9,31 +9,41 @@ $('dt').on('click', function(){
 })
 
 $('#bt1').on('click', function(){
-    let lastLi1 = $('#national-parks').children()
-    let lastLi2 = $('#state-parks-texas').children()
-    let lastLi3 = $('#randomN').children()
-    lastLi1.last().toggleClass('highlight')
-    lastLi2.last().toggleClass('highlight')
-    lastLi3.last().toggleClass('highlight')
+    // first try
+    // let lastLi1 = $('#national-parks').children()
+    // let lastLi2 = $('#state-parks-texas').children()
+    // let lastLi3 = $('#randomN').children()
+    // lastLi1.last().toggleClass('highlight')
+    // lastLi2.last().toggleClass('highlight')
+    // lastLi3.last().toggleClass('highlight')
+    $('ul').each(function (){
+        $(this).find('li').last().toggleClass('highlight')
+    })
 })
 
-$('#nationalH').on('click', function(){
-    let bold1 = $('#national-parks').children()
-    $(this).each(function(){
-            bold1.toggleClass('bold')
-        })
-})
-$('#stateH').on('click', function(){
-    let bold2 = $('#state-parks-texas').children()
-    $(this).each(function(){
-        bold2.toggleClass('bold')
-    })
-})
-$('#randomH').on('click', function(){
-    let bold3 = $('#randomN').children()
-    $(this).each(function(){
-        bold3.toggleClass('bold')
-    })
+
+// first try
+// $('#nationalH').on('click', function(){
+//     let bold1 = $('#national-parks').children()
+//     $(this).each(function(){
+//             bold1.toggleClass('bold')
+//         })
+// })
+// $('#stateH').on('click', function(){
+//     let bold2 = $('#state-parks-texas').children()
+//     $(this).each(function(){
+//         bold2.toggleClass('bold')
+//     })
+// })
+// $('#randomH').on('click', function(){
+//     let bold3 = $('#randomN').children()
+//     $(this).each(function(){
+//         bold3.toggleClass('bold')
+//     })
+// })
+
+$('h3').on('click', function (){
+    $(this).next().children().toggleClass('bold')
 })
 
 $('li').on('click', function(){
