9f0ae5d6ef63bab70089d5b5d43863d3ca1ee39a
Aaron Bond <aaron.reid.bond@gmail.com>
2023-06-07 14:08:56 -0700

    promises exercise1

--- js/promises.js
+++ js/promises.js
@@ -23,21 +23,21 @@ fetch('https://api.github.com/repos/aaron-reid-bond/codeup-web-exercises/commits
 
 // my failure
 
-// function wait(x){
-//     const myPromise = new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if (Math.random() > 0.5) {
-//                 resolve();
-//             } else {
-//                 reject();
-//             }
-//         }, x);
-//     });
-//     console.log(myPromise);
-// }
-//
-// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+function wait(x){
+    const myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.5) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, x);
+    });
+    return myPromise
+}
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
 // lesson sample
